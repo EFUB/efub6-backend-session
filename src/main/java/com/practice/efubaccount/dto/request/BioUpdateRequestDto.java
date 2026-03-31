7ddedb8f77e38dd3f63d32b332d894efbf08681f
Zin Wai Yan <zinwaiyan2226@gmail.com>
2026-03-31 21:21:33 +0900

    [feat] CRUD 실습

--- src/main/java/com/practice/efubaccount/dto/request/BioUpdateRequestDto.java
+++ src/main/java/com/practice/efubaccount/dto/request/BioUpdateRequestDto.java
@@ -1,8 +1,11 @@
 package com.practice.efubaccount.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class BioUpdateRequestDto {
+    @NotBlank
+    private String bio;
 
 }
