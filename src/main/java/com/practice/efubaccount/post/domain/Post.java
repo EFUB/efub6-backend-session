fca1d1a6ac078e774d4358ad91d686f37b742852
KKANGCHONG <leee0414@naver.com>
2026-04-09 16:44:57 +0900

    ✨feat: 3주차 세미나 실습 완료

--- src/main/java/com/practice/efubaccount/post/domain/Post.java
+++ src/main/java/com/practice/efubaccount/post/domain/Post.java
@@ -1,27 +1,43 @@
-//package com.practice.efubaccount.post.domain;
-//
-//import com.practice.efubaccount.account.domain.Account;
-//import com.practice.efubaccount.global.domain.BaseEntity;
-//import jakarta.persistence.*;
-//import lombok.AccessLevel;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//@Entity
-//@Getter
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
-//public class Post{
-//
-//    @Builder
-//    public Post(String title, Account writer, String content) {
-//        this.title = title;
-//        this.writer = writer;
-//        this.content = content;
-//        this.viewCount = 0L;
-//    }
-//
-//    public void changeContent(String newContent) {
-//        this.content = newContent;
-//    }
-//}
+package com.practice.efubaccount.post.domain;
+
+import com.practice.efubaccount.account.domain.Account;
+import com.practice.efubaccount.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // 아이디값 직접 직정 안 해줘도 됨
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account writer;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Long viewCount;
+
+    @Builder
+    public Post(String title, Account writer, String content) {
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+        this.viewCount = 0L;
+    }
+
+    public void changeContent(String newContent) {
+        this.content = newContent;
+    }
+}
