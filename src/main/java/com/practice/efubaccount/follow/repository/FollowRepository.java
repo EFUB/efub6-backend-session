1bdabdf30fed47aebb9931e92dad42a4da4515e5
전채연 <ami02074657@gmail.com>
2026-05-19 23:17:47 +0900

    [Feat] week6 세미나 실습

--- src/main/java/com/practice/efubaccount/follow/repository/FollowRepository.java
+++ src/main/java/com/practice/efubaccount/follow/repository/FollowRepository.java
@@ -11,5 +11,13 @@ import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
+    boolean existsByFollowerAndFollowee(Account requester, Account target);
 
+    Optional<Follow> findByFollowerAndFollowee(Account follower, Account followee);
+
+    @Query("SELECT f FROM Follow f JOIN FETCH f.follower WHERE f.followee = :requester")
+    List<Follow> findAllByFollowee(@Param("requester") Account followee);
+
+    @Query("SELECT f FROM Follow f JOIN FETCH f.followee WHERE f.follower = :requester")
+    List<Follow> findAllByFollower(@Param("requester") Account follower);
 }
